3c4e1d39eeaddba66418432706503de3131f02a4
Narayan <narayanrajdubey9889@gmail.com>
2025-12-26 23:42:36 +0530

    updated backend connectivity link from the render

--- client-web/src/api/axios.js
+++ client-web/src/api/axios.js
@@ -1,15 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
+const BASE_URL = "https://YOUR-APP-NAME.onrender.com/api/v1";
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api/v1',
+  // baseURL: 'http://localhost:5000/api/v1',
+  baseURL: BASE_URL,
 });
 
 // Automatically add Token to headers if it exists
 API.interceptors.request.use((req) => {
-    if (localStorage.getItem('token')) {
-        req.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
-    }
-    return req;
+  if (localStorage.getItem("token")) {
+    req.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+  }
+  return req;
 });
 
 export default API;
